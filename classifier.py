1fcdd07d2156d40bdf2d921545d8a7d5cf02906b
Costanza <costanza.volpini@gmail.com>
2020-05-10 11:15:56 +0200

    added python files

---
+++ classifier.py
@@ -0,0 +1,130 @@
+import keras
+import skimage
+from keras.models import Sequential, load_model, model_from_json
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Lambda, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from keras import backend as K 
+import json
+import os
+import numpy as np
+from utils import replicate
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, x, y, extend_max=6, batch_size=64, shuffle=True):
+        'Initialization'
+        self.batch_size = batch_size
+
+        self.image_gen = ImageDataGenerator(
+            rotation_range=360,
+            width_shift_range=0.1,
+            height_shift_range=0.1,
+            zoom_range=[0.8, 1.8]
+        ).flow(x, y=y, batch_size=batch_size, shuffle=shuffle)
+
+        self.extend_max = extend_max
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return len(self.image_gen)
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Get data
+        X, y = self.image_gen[index]
+
+        # Add some random padding
+        X = replicate(
+            X,
+            left=np.random.randint(self.extend_max),
+            right=np.random.randint(self.extend_max),
+            up=np.random.randint(self.extend_max),
+            down=np.random.randint(self.extend_max)
+        )
+        
+        # Add noise on half of the samples in the batch
+        half_batch = self.batch_size//2
+        X[:half_batch] = skimage.util.random_noise(X[:half_batch], mode="gaussian", mean=0, var=0.05, clip=False)
+        
+        return X, y
+
+    def on_epoch_end(self):
+        self.image_gen.on_epoch_end()
+        
+        
+        
+############ MODEL ###############
+class CNNModel:
+    def __init__(self, model_dir='.', num_classes=14, load=False, verbose=True):
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        self.json_model_file=model_dir + '/json_model.json'
+        self.model_weights_file=model_dir + '/model_weights.h5'
+
+        if load:
+            self.load()
+        else:
+            self.model = Sequential([
+                # Conv layers
+                Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same', input_shape=(None, None, 1)),
+                BatchNormalization(),
+                MaxPooling2D(pool_size=(2,2)),
+                Dropout(0.20),
+
+                Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same'),
+                BatchNormalization(),
+                MaxPooling2D(pool_size=(2,2)),
+                Dropout(0.20),
+
+                Conv2D(filters=512, kernel_size=(3,3), activation='relu', padding='same'),
+                BatchNormalization(),
+                MaxPooling2D(pool_size=(2,2)),
+                Dropout(0.20),
+
+                # Flatten(),
+                Lambda(lambda x: K.mean(x, axis=[1, 2])),
+
+                # Densely connected layers
+                Dense(128, activation='relu'),
+                BatchNormalization(),
+
+                # Output layer
+                Dense(num_classes, activation='softmax')
+            ])
+
+            # compile with adam optimizer & categorical_crossentropy loss function
+            self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+            
+        if verbose:
+            print(self.model.summary())
+    
+    def fit(self, x, y, epochs=50, batch_size=64):
+        # get train and validation splits
+        x_train, x_valid, y_train, y_valid = train_test_split(x, y, test_size=0.10, shuffle=True)
+
+        # instantiate the data generator
+        train_generator = DataGenerator(x_train, y=y_train, batch_size=batch_size)
+
+        # train
+        self.history = self.model.fit(train_generator, epochs=epochs, validation_data=(x_valid, y_valid))
+        
+    def save(self):
+        # save the model architecture
+        with open(self.json_model_file, "w") as json_file:
+            json.dump(self.model.to_json(), json_file)
+
+        # save the weights
+        self.model.save_weights(self.model_weights_file)
+        
+    def load(self):
+        # load the model architecture
+        with open(self.json_model_file,'r') as f:
+            json_model = json.load(f)
+
+        # load the weights
+        self.model = model_from_json(json_model)
+        self.model.load_weights(self.model_weights_file)
+        
+        # compile with adam optimizer & categorical_crossentropy loss function
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
---
+++ utils.py
@@ -0,0 +1,85 @@
+from keras.utils import to_categorical
+import numpy as np
+
+def remove_9(x, y):
+    mask = y != 9
+    return x[mask], y[mask]
+
+def replicate(X, left=13, right=13, up=21, down=21):
+    """
+    Given a batch of images (of shape (num samples, image height, image width, num channels)),
+    replicates all the images' border on the 4 sides.
+    """
+    # left
+    X = np.concatenate((np.tile(X[:, :, :1], (1, 1, left, 1)), X), axis=2)
+
+    # right
+    X = np.concatenate((X, np.tile(X[:, :, -1:], (1, 1, right, 1))), axis=2)
+
+    # up
+    X = np.concatenate((np.tile(X[:, :1, :], (1, up, 1, 1)), X), axis=1)
+
+    # down
+    X = np.concatenate((X, np.tile(X[:, -1:, :], (1, down, 1, 1))), axis=1)
+
+    return X
+
+# def replicate_img(x, left=13, right=13, up=21, down=21):
+#     """
+#     Given a batch of images (of shape (num samples, image height, image width, num channels)),
+#     replicates all the images' border on the 4 sides.
+#     """
+#     # left
+#     x = np.concatenate((np.tile(x[:, :1], (1, left, 1)), x), axis=1)
+
+#     # right
+#     x = np.concatenate((x, np.tile(x[:, -1:], (1, right, 1))), axis=1)
+
+#     # up
+#     x = np.concatenate((np.tile(x[:1, :], (up, 1, 1)), x), axis=0)
+
+#     # down
+#     x = np.concatenate((x, np.tile(x[-1:, :], (down, 1, 1))), axis=0)
+
+#     return x
+
+def preprocessing_mnist_data(train_digits, train_labels, test_digits, test_labels, num_classes=14):
+    image_height = train_digits.shape[1]  
+    image_width = train_digits.shape[2]
+    num_channels = 1  # we have grayscale images
+
+    # re-shape the images data
+    train_data = np.reshape(train_digits, (train_digits.shape[0], image_height, image_width, num_channels))
+    test_data = np.reshape(test_digits, (test_digits.shape[0],image_height, image_width, num_channels))
+
+    # re-scale the image data to values between (0.0,1.0]
+    train_data = train_data.astype('float32') / 255.
+    test_data = test_data.astype('float32') / 255.
+
+    # add padding to have image similar to our images
+    train_data = replicate(train_data)
+    # train_data = normalize(train_data, batch=True)
+    test_data = replicate(test_data, left=16, right=16, up=24, down=24)
+    # test_data = normalize(test_data, batch=True)
+
+    # one-hot encode the labels - we have 9 output classes (esclude 9!)
+    # so 3 -> [0 0 0 1 0 0 0 0 0 0], 5 -> [0 0 0 0 0 1 0 0 0 0] & so on
+    train_labels_cat = to_categorical(train_labels, num_classes)
+    test_labels_cat = to_categorical(test_labels, num_classes)
+    return train_data, train_labels_cat, test_data, test_labels_cat
+
+def preprocessing_our_data(objects):
+    toRecognize = []
+
+    for im in objects:
+        im_01 = (1 - im.astype('float32') / 255.)
+        toRecognize.append(im_01)
+        # toRecognize.append(normalize(im_01))
+
+    toRecognize = np.stack(toRecognize)
+    return toRecognize
+
+def normalize(x, batch=False):
+    if batch:
+        return (x - x.mean(axis=(1, 2)).reshape(x.shape[0], 1, 1, 1))  / x.std(axis=(1, 2)).reshape(x.shape[0], 1, 1, 1)
+    return np.array((x - x.mean()) / x.std())
